8e26ead541709b4571658139c01d4b1bce1c35a3
oppster <len.nicodemus@gmail.com>
2026-05-26 15:20:13 -0400

    Update download-workbook.js

--- api/download-workbook.js
+++ api/download-workbook.js
@@ -86,9 +86,10 @@ export default async function handler(req, res) {
         
             await logDownloadEvent({
                 email: "UNKNOWN",
-                license_key: 
-                  token?.substring(0,8) + "..."
-                  : "UNKNOWN",
+                license_key:
+                  token
+                    ? token.substring(0,8) + "..."
+                    : "UNKNOWN",
                 tier: "UNKNOWN",
                 download_ip: ipAddress,
                 download_country: downloadCountry,
